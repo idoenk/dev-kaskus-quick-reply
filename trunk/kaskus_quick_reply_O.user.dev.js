9fb2cb41a76ef340bd01fd152cb65e4eb5697de4
1d03nk@gmail.com <1d03nk@gmail.com@efb66246-df73-5552-6b5b-f032c8b2573e>
2013-05-19 20:13:19 +0000

    KQR (O) v4.1.0.6 - 2013-05-20 . 1368989414499  Forked version from 4.1.0.6 (adapting Opera)  Fix multiquote

--- trunk/kaskus_quick_reply_O.user.dev.js
+++ trunk/kaskus_quick_reply_O.user.dev.js
@@ -10,8 +10,8 @@
 // @license        (CC) by-nc-sa 3.0
 // @exclude        *kaskus.co.id/post_reply/*
 // @version        4.1.0.6
-// @dtversion      1304234106
-// @timestamp      1366657711740
+// @dtversion      1305204106
+// @timestamp      1368989414499
 // @description    provide a quick reply feature, under circumstances capcay required.
 // @author         idx(302101; http://userscripts.org/users/idx); bimatampan(founder);
 // @contributor    s4nji, riza_kasela, p1nky, b3g0, fazar, bagosbanget, eric., bedjho, Piluze, intruder.master, Rh354, gr0, hermawan64, slifer2006, gzt, Duljondul, reongkacun, otnaibef, ketang8keting, farin, drupalorg, .Shana, t0g3, & all-kaskuser@t=3170414
@@ -29,7 +29,7 @@
 //
 // -!--latestupdate
 //
-// v4.1.0.6 - 2013-04-23 . 1366657711740
+// v4.1.0.6 - 2013-05-20 . 1368989414499
 //  Forked version from 4.1.0.6 (adapting Opera)
 //
 // -/!latestupdate---
@@ -60,10 +60,10 @@ var gvar=function(){}, isQR_PLUS = 0; // purpose for QR+ pack, disable stated as
 // gvar.scriptMeta.scriptID
 gvar.sversion = 'v' + '4.1.0.6';
 gvar.scriptMeta = {
-  timestamp: 1366657711740 // version.timestamp
+  timestamp: 1368989414499 // version.timestamp
   //timestamp: 999 // version.timestamp for test update
   
-  ,dtversion: 1304234106 // version.date
+  ,dtversion: 1305204106 // version.date
   
   ,titlename: 'Quick Reply' + ( isQR_PLUS !== 0 ? '+' : '' )
   ,scriptID: 80409 // script-Id
@@ -621,7 +621,11 @@ var rSRC = {
       +'</div>' // #custom_bottom
 
       +'<div id="scustom_container" style="max-width: 829px;">'
-      + '<div style="margin:8px 0">Custom Smiley Not Found</div>'
+      + '<div style="margin:8px 0">'
+      +  'Custom Smiley Not Found, <a href="http://goo.gl/vBPK8" target="_blank">what is this?</a>'
+      +  '<br/><br/>'
+      +  'Browse to <a href="http://kask.us/gWtme" target="_blank">Emoticon Corner</a>'
+      + '</div>'
       +'</div>'
 
       // manage | add_group properties
@@ -3835,8 +3839,8 @@ var _QQparse = {
         }
         openTag = (mct && mct[1]);
         if( openTag ){
-          mct[1] = mct[1].toUpperCase();
-          clog('bbcode recognized: ['+mct[1].toUpperCase()+']');
+          mct[1] = (isLink ? mct[1] : mct[1].toUpperCase());
+          clog('bbcode recognized: ['+mct[1]+']');
         }
         lastIdx = LT.a.length-1;
         pRet = (mct && mct[1] ? (isDefined(LT.a[lastIdx]) ? '['+LT.a[lastIdx].toUpperCase()+'='+mct[1]+']':'') : (isDefined(LT.a[lastIdx]) ? '['+'/'+LT.a[lastIdx].toUpperCase()+']' : '') );
